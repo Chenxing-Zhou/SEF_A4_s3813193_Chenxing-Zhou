8ca53b8c788a56d0fdcfd7b325d97ff1967a868d
Chenxing-Zhou <s3813193@student.rmit.edu.au>
2022-06-01 00:25:06 +0800

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,50 @@
+ public class Food {
+
+     private String foodID;
+
+     private String foodName;
+
+     private String foodDescription;
+
+     private double foodPrice;
+
+     private int foodCalorie;
+
+     private String foodType;
+
+     public Food (...)
+
+     {
+
+      }
+
+     public boolean AddFood(string)
+     writer
+     {
+          
+          // add the information of food to a TXT file
+
+         // if the food information is added to the TXT file, it should return true;
+
+          // if the food information cannot be added to the TXT file, it should return false;
+
+     }
+     public boolean checkname (string)
+        if (name.length() > 5, name.length()< 30);
+        	return true;
+        else
+        	return false;
+
+     public boolean UpdateFood(...)
+
+     {
+
+          //  Update the information of a given food in a TXT file
+
+         // if the food information is updated in the TXT file, it should return true;
+
+         // if the food information cannot be updated in the TXT file, it should return false;
+
+     }
+
+}
